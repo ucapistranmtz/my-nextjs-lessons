2a32a258436661097c3696e8ee446c4a6009da78
Ulises Capistrán <ucapistranmtz@gmail.com>
2025-06-19 07:24:41 -0500

    ✨ lesson: Iconos y ruta activa

--- 2-dashboard-example/app/page.tsx
+++ 2-dashboard-example/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('dashboard/counter');
+  redirect('dashboard/main');
   return (
     <>
       <h1>Hello World</h1>
--- 2-dashboard-example/next.config.ts
+++ 2-dashboard-example/next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",}
+      ]
+    },
   /* config options here */
 };
 
