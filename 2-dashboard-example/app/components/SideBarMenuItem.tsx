3992cd52c03fa59b0d0c81b41c3d4961404f0f9f
Ulises Capistrán <ucapistranmtz@gmail.com>
2025-06-22 08:00:34 -0500

    ✨ lesson: solucion a la tarea

--- 2-dashboard-example/app/components/SideBarMenuItem.tsx
+++ 2-dashboard-example/app/components/SideBarMenuItem.tsx
@@ -1,3 +1,6 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React, { JSX } from 'react';
 interface Props {
   path: string;
@@ -7,31 +10,21 @@ interface Props {
 }
 
 export const SideBarMenuItem = ({ path, icon, title, subTitle }: Props) => {
+  const currentPath = usePathname();
   return (
-    <a
-      href='#'
-      className='w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 bg-blue-800 hover:bg-white/5 transition ease-linear duration-150'
+    <Link
+      href={path}
+      className={`w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3  hover:bg-white/5 transition ease-linear duration-150
+       ${currentPath === path ? 'bg-blue-8000' : ''} 
+       `}
     >
       <div>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke-width='1.5'
-          stroke='currentColor'
-          className='w-6 h-6 text-white'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z'
-          />
-        </svg>
+        <div>{icon}</div>
       </div>
       <div className='flex flex-col'>
-        <span className='text-lg font-bold leading-5 text-white'>Dashboard</span>
-        <span className='text-sm text-white/50 hidden md:block'>Data Overview</span>
+        <span className='text-lg font-bold leading-5 text-white'>{title}</span>
+        <span className='text-sm text-white/50 hidden md:block'>{subTitle}</span>
       </div>
-    </a>
+    </Link>
   );
 };
