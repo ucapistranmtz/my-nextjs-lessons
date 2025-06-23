918294f2b9171714f783e6efd2db4ef9387e9f03
Ulises Capistrán <ucapistranmtz@gmail.com>
2025-06-23 09:58:21 -0500

    ✨ end of section 4

--- 2-dashboard-example/app/dashboard/counter/page.tsx
+++ 2-dashboard-example/app/dashboard/counter/page.tsx
@@ -1,7 +1,15 @@
+import { CartCounter } from '@/app/shopping-cart';
+
+export const metadata = {
+  title: 'Counter Page',
+  description: 'A simple counter page to demonstrate state management in Next.js',
+};
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className=' flex flex-col items-center justify-center w-full h-full'>
+      <span>Shopping car items </span>
+      <CartCounter value={20} />
     </div>
   );
 }
