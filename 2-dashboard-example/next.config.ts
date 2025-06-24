5b46df30067bae1329ade049a5a19620a4bf08a4
Ulises <ucapistran@outlook.com>
2025-06-24 07:51:48 -0500

    testing

--- 2-dashboard-example/next.config.ts
+++ 2-dashboard-example/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
+        protocol: "https", 
         hostname: "images.unsplash.com",}
       ]
     },
