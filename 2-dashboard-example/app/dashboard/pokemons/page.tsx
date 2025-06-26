cbbd5e4527d178bcded7bc48b5c6803cca20959c
Ulises <ucapistranmtz@gmail.com>
2025-06-26 17:40:37 -0500

    error page

--- 2-dashboard-example/app/dashboard/pokemons/page.tsx
+++ 2-dashboard-example/app/dashboard/pokemons/page.tsx
@@ -8,7 +8,7 @@ const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => 
     id: pokemon.url.split('/').at(-2)!,
     name: pokemon.name,
   }));
-
+ throw new Error('temporary error')
   return pokemons;
 };
 
